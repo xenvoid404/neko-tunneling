a97c46350cfceb3dce08b598f9147b1252f639c6
xenvoid404 <xenvoid404@gmail.com>
2026-07-26 19:51:18 +0700

    refactor utilitas random

--- api/pkg/utils/random.go
+++ api/pkg/utils/random.go
@@ -3,21 +3,26 @@ package utils
 import (
 	"crypto/rand"
 	"encoding/hex"
+	"fmt"
 
 	"github.com/google/uuid"
 )
 
 func RandomUsername() string {
-	bytes := make([]byte, 4)
-	_, _ = rand.Read(bytes)
-	return "trial-" + hex.EncodeToString(bytes)
+	return "trial-" + randomHex(4)
 }
 
 func RandomPassword(protocol string) string {
 	if protocol == "ssh" {
-		bytes := make([]byte, 4)
-		_, _ = rand.Read(bytes)
-		return hex.EncodeToString(bytes)
+		return randomHex(4)
 	}
 	return uuid.New().String()
+}
+
+func randomHex(n int) string {
+	b := make([]byte, n)
+	if _, err := rand.Read(b); err != nil {
+		panic(fmt.Errorf("crypto/rand gagal: %w", err))
+	}
+	return hex.EncodeToString(b)
 }
